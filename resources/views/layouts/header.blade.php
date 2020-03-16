c665f14566dc78cd713d49381bf9087e120bd07c
Kautsar Al Bana <kautsaralbana@hotmail.com>
2020-03-16 17:46:25 +0700

    Update Service Panel

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -32,7 +32,7 @@
                 {{-- Profile --}}
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{asset('assets/images/users/1.jpg')}}" alt="user"
-                          class="profile-pic" /></a>
+                        class="profile-pic" /></a>
                     <div class="dropdown-menu dropdown-menu-right scale-up">
                         <ul class="dropdown-user">
                             <li>
@@ -50,7 +50,7 @@
                             <li role="separator" class="divider"></li>
                             <li>
                                 <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">
+                                            document.getElementById('logout-form').submit();">
                                     <i class="fa fa-power-off"></i> {{ __('Logout') }}</a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
